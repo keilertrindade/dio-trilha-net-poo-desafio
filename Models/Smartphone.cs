60457438e51164471750b7a78d462ea3c7334d2f
Keiler Trindade <keiler.trindade@hotmail.com>
2024-05-27 20:46:37 -0300

    Add files via upload
    Adição de arquivos.

---
+++ Models/Smartphone.cs
@@ -0,0 +1,37 @@
+namespace DesafioPOO.Models
+{
+    public abstract class Smartphone
+    {
+        public string Numero { get; set; }
+        private string  Modelo { get; set; }
+        private string Imei { get; set; }
+        private int Memoria { get; set; }
+        
+
+        public Smartphone(string numero, string modelo, string imei, int memoria)
+        {
+            Numero = numero;
+            Modelo = modelo;
+            Imei = imei;
+            Memoria = memoria;
+        }
+
+        protected Smartphone(string numero)
+        {
+            Numero = numero;
+        }
+
+
+        public void Ligar()
+        {
+            Console.WriteLine("Ligando...");
+        }
+
+        public void ReceberLigacao()
+        {
+            Console.WriteLine("Recebendo ligação...");
+        }
+
+        public abstract void InstalarAplicativo(string nomeApp);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using DesafioPOO.Models;
+
+
+Console.WriteLine("Criando smartphone Iphone:");
+Iphone iphone = new Iphone (numero: "992232342345", modelo: "Iphone 13 Pro Max", imei: "3512344546767667",  memoria:128);
+iphone.InstalarAplicativo("Instagram");
+
+Console.WriteLine("Criando smartphone Nokia:");
+Nokia nokia = new Nokia("559685968596","Nokia 3405","12987126912612",65);
+nokia.InstalarAplicativo("EA Football 24");
